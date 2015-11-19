9a88effbba13b11be3b49bd94b1ec9e8403857c3
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-11-19 14:25:09 +0100

    config models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,6 @@
 class Reservation < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :event
+
+  validate_presence_of :paid, :nb_reservation, :user_id, :event_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  has_many :events
+  has_many :reservations
+
+  validate_presence_of :name, :firstname, :nickname, :phone
+  validates :phone, length: { is: 10 }
+  validates_uniqueness_of :nickname
 end
