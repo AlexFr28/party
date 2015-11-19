3b1d6b68cd76806bf1ac16f19f45e5a50e7049ec
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-11-19 11:26:39 +0100

    add reservation table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119101609) do
+ActiveRecord::Schema.define(version: 20151119102529) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,21 @@ ActiveRecord::Schema.define(version: 20151119101609) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "reservations", force: :cascade do |t|
+    t.boolean  "paid"
+    t.float    "penalty"
+    t.integer  "nb_reservation"
+    t.integer  "note"
+    t.string   "message"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "reservations", ["event_id"], name: "index_reservations_on_event_id"
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                                   null: false
