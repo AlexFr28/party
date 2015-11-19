df1cb96153bd9b7ef84115c5dd10037dc12dfb7e
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-11-19 11:27:56 +0100

    add private event table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119102529) do
+ActiveRecord::Schema.define(version: 20151119102717) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20151119102529) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "private_events", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "private_events", ["event_id"], name: "index_private_events_on_event_id"
+  add_index "private_events", ["user_id"], name: "index_private_events_on_user_id"
 
   create_table "reservations", force: :cascade do |t|
     t.boolean  "paid"
