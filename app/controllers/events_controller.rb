8f7806e762599ffa76b0597561a851a50cc64b93
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-11-19 19:30:26 +0100

    form_for event + other

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,9 +4,19 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new()
   end
 
   def create
+    # binding.pry
+    @event = Event.new(event_param)
+    @event.date = DateTime.strptime(params["event"]["date"], "%Y-%m-%d")
+    @event.limit_payment = DateTime.strptime(params["event"]["limit_payment"], "%Y-%m-%d")
+    if @event.save
+      redirect_to users_path, notice: "\"#{@event.name}\" enregistré"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -19,10 +29,19 @@ class EventsController < ApplicationController
   end
 
   def show
+    @user = current_user
     @event = Event.find(params[:id])
     @rest = "illimité"
     if @event.nb_person != 0
       @rest = "#{}/#{@event.nb_person}"
     end
+    @resa = Reservation.where(user_id: current_user.id, event_id: @event.id)
   end
+
+  private
+
+  def event_param
+    params.require(:event).permit(:name, :address, :date, :theme, :price_per_person, :nb_person, :limit_payment, :private_event, :user_id, :description, :image)
+  end
+
 end
