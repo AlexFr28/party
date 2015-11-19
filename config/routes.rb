fd13facc74198b15a4eeae46d94c31462b212726
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-11-19 12:34:29 +0100

    add events seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,12 @@
 Rails.application.routes.draw do
-  get 'events/index'
-
-  get 'events/new'
-
-  get 'events/create'
-
-  get 'events/edit'
-
-  get 'events/update'
-
-  get 'events/delete'
-
-  get 'events/show'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/delete'
-
-  get 'users/show'
-
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -42,7 +15,8 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
+  resources :events
 
   # Example resource route with options:
   #   resources :products do
